c4bb14f6ab4ed3e32fb8c17bd22683a543608ffc
Nicholas Karch <nckarch@gmail.com>
2021-05-14 09:16:52 -0400

    Initial Commit

---
+++ Inhale/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Inhale
+//
+//  Created by Nick Karch on 5/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
